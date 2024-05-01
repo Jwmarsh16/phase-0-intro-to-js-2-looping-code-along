00274e0042e3ad1be39633707d07e0e342091844
Jonathan Marshall <jwmarsh16@gmail.com>
2024-05-01 16:01:54 -0500

    Lab Complete

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Code your solutions in this file
+
+
+
+
+function writeCards(names, event) {
+    const thankYouMessages = [];
+    for (let i = 0; i < names.length; i++)
+    { 
+        const message = `Thank you, ${names[i]}, for the wonderful ${event} gift!`; 
+        thankYouMessages.push(message); 
+    
+    }
+
+    return thankYouMessages; 
+}
+
+
+console.log(writeCards(["Charlie", "Samip", "Ali"], "birthday"));
+
+
+
+function countDown() {
+    let number = 10; 
+    while (number >= 0) {
+        console.log(number);
+        number--;
+    }
+}
+
+countDown(5);
